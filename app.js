380406f39721d45d590d55c4c82ce4c919fdd3d8
Huzaifa Malick <135388170+Huzaifamalic@users.noreply.github.com>
2023-12-07 20:04:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+//Assignment 3
+var arr = ["Huzaifa Malick","bsc22",22]
+arr.push("karacki")
+arr.shift
+arr.unshift(55)
+arr.pop
+arr.splice(1,)
+document.write(arr )
+var arr = arr.slice(1,4)
+document.write(arr)
